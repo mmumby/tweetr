4769d9d3f9ece764ebdb6c1ad269c20cc98e4c99
Meredith <meredith.mumby@outlook.com>
2017-07-12 03:43:18 +0000

    Forgot to commit today, Updated CSS to include tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+
+//function to count text input
+// counter turns red when text input goes over 140
+
+$(document).ready(function(){
+  var maxCount = 140;
+  $('textarea').on('keyup', function(){
+    var currentValue = (maxCount - this.value.length);
+    var countDown = this.value.length;
+
+    if (countDown >= maxCount) {
+      $(this).parent().children('.counter').addClass('counter-negative');
+      $(this).parent().children('.counter').text(currentValue);
+    } else if (countDown < maxCount) {
+      $(this).parent().children('.counter').removeClass('counter-negative');
+      $(this).parent().children('.counter').text(currentValue);
+    }
+  });
+});
